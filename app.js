4040024f34df4891460ef1f8107514f4462b6b2f
Shingo Yabuki <bryanprsvar@gmail.com>
2016-01-11 02:40:05 -0300

    creacion de la extructura webapp

---
+++ app.js
@@ -0,0 +1,19 @@
+var express = require('express');
+
+var app = express();
+
+var port = 3030;
+
+app.use(express.static('public'));
+app.use(express.static('src/views'))
+
+
+app.get('/',function(req, res){
+    res.send("Hola mundo");
+});
+app.get('/books', function (req, res) {
+    res.send('Hola Books');
+})
+app.listen(port, function (err) {
+    console.log('corriendo servidor en el puerto ' + port);
+});
